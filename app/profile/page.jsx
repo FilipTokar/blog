bba6a203787fc2d9d4dfe585520cb303bdb0bd59
Filip <filip.tokar@seznam.cz>
2023-07-05 10:08:21 +0200

    profileInfo.jsx update and profile page markup

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,12 +1,22 @@
-import ProfileInfo from '@/components/ProfileInfo'
-import React from 'react'
+import Post from "@/components/Post";
+import ProfileInfo from "@/components/ProfileInfo";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <ProfileInfo/>
+      <ProfileInfo />
+      <h2 className="text-5xl my-10">My posts</h2>
+      <div className="grid grid-cols-3 gap-6">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
